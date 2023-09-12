e257f560099a6468cd7217ee20cc4dd82082d897
szt2otokov <szt2otokov@vasvari.org>
2023-09-12 13:06:21 +0200

    dun

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int sum = 0;
+        for(int i = 0; i < 5;i++){
+            System.out.print("Adj meg egy számot: ");
+            sum += scn.nextInt();
+        }
+        System.out.println("A beírt számok összege: " + sum);
+        System.out.println("Átlaguk: " + (float)sum/5);
+
+
+    }
+}
